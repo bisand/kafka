03cabaaa4b77f2afb18102a499ecd77d30d0284b
André Biseth <andre@biseth.net>
2023-10-30 21:17:05 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Hostname: ${HOSTNAME}, Replica: ${REPLICA}, Replicas: ${REPLICAS}, Share dir: ${SHARE_DIR}"
+echo "Hostname: ${HOSTNAME}, Replica: ${REPLICA}, Replicas: ${REPLICAS}, Share dir: ${SHARE_DIR} "
 
 NODE_ID=$((REPLICA - 1))
 
